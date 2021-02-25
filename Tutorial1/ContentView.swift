6d31a3d51cfae4811eeea417ae77be136a619641
CarlosDeBenito <carlosde@gmai.com>
2021-02-25 10:13:52 +0100

    Initial Commit

---
+++ Tutorial1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tutorial1
+//
+//  Created by Carlos de benito on 25/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tutorial1/Tutorial1App.swift
@@ -0,0 +1,17 @@
+//
+//  Tutorial1App.swift
+//  Tutorial1
+//
+//  Created by Carlos de benito on 25/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Tutorial1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
